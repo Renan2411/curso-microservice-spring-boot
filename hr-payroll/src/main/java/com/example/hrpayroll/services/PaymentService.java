8eaf606245a50c9752f48426c5f835021182c761
renan <renan.duarte@azi.com.br>
2025-04-28 21:24:32 -0400

    [COMMIT] - Adicionando Feing Client

--- hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/services/PaymentService.java
@@ -2,6 +2,7 @@ package com.example.hrpayroll.services;
 
 import com.example.hrpayroll.entities.Payment;
 import com.example.hrpayroll.entities.Worker;
+import com.example.hrpayroll.feingclients.WorkerFeingClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,10 +17,19 @@ public class PaymentService {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WorkerFeingClient workerFeingClient;
+
     @Value("${hr-worker.host}")
     private String workerHost;
 
     public Payment getPayment(Long workerId, Integer days) {
+        Worker worker = workerFeingClient.buscarPorId(workerId).getBody();
+
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
+    }
+
+    public Payment getPaymentRestTemplate(Long workerId, Integer days) {
         Map<String, String> uriVariables = new HashMap<>();
         uriVariables.put("id", workerId.toString());
 
