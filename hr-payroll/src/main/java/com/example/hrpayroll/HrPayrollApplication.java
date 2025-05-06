c0a7baf9c988d490fe4b6dd70b6790a5cffa7ecb
renan.duarte <renan.duarte@azi.com.br>
2025-05-05 21:51:06 -0400

    [COMMIT]: Configurando conexões projeto hr-eureka-server

--- hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/example/hrpayroll/HrPayrollApplication.java
@@ -2,11 +2,13 @@ package com.example.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "hr-worker")
+//@RibbonClient(name = "hr-worker") COM O EUREKA NÃO HÁ NECESSIDADE DO RIBBON
 @EnableFeignClients
+@EnableEurekaClient
 @SpringBootApplication
 public class HrPayrollApplication {
 
