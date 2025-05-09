ec68590f148ed46c8a94cd151fd86cb776eb71b1
renan.duarte <renan.duarte@azi.com.br>
2025-05-08 21:32:32 -0400

    [COMMIT]: Criando projeto hr-gateway-zuul

---
+++ hr-gateway-zuul/src/test/java/com/example/hrgatewayzuul/HrGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hrgatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
