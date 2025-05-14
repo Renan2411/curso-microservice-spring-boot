b3131028ffbe06ecfa533cfe70586bab1eb0a3d2
renan.duarte <renan.duarte@azi.com.br>
2025-05-13 21:19:31 -0400

    Revert "[COMMIT]: Finalizando modulo"
    This reverts commit 7a329ec891140ef5b57780f99bffb122ab3745ec.

--- hr-worker/src/main/java/org/example/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/org/example/hrworker/resources/WorkerResource.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RefreshScope
 @RequestMapping("/workers")
 public class WorkerResource {
 
@@ -37,10 +35,10 @@ public class WorkerResource {
     private WorkerRepository workerRepository;
 
     @GetMapping(value = "/configs")
-    public ResponseEntity<String> buscarConfiguracoes() {
+    public ResponseEntity<Void> buscarConfiguracoes() {
         logger.info("config = " + testConfig);
 
-        return new ResponseEntity<>(testConfig, HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @GetMapping
