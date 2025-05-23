8d669bb7e9d2204d29b8d2f967bce9fbff7f7722
renan.duarte <renan.duarte@azi.com.br>
2025-05-22 21:28:44 -0400

    [COMMIT]: Adicionando configurações para geração de token e autenticação

--- hr-oauth/src/main/java/com/example/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/example/hroauth/services/UserService.java
@@ -3,13 +3,16 @@ package com.example.hroauth.services;
 import com.example.hroauth.entities.User;
 import com.example.hroauth.feignclients.UserFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 import java.util.logging.Logger;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private static final Logger logger = Logger.getLogger(UserService.class.getName());
 
@@ -23,11 +26,16 @@ public class UserService {
         if (Objects.isNull(user)) {
             logger.warning("Email Not found = " + email);
 
-            throw new IllegalArgumentException("Email not found");
+//            throw new IllegalArgumentException("Email not found");
+            throw new UsernameNotFoundException("Email not found");
         }
 
-        logger.info("Email Found = " + user.toString());
+        logger.info("Email Found = " + user.getEmail());
         return user;
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return this.findByEmail(email);
+    }
 }
